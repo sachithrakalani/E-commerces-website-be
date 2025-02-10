54f9be74fbbb22c872d33c05ef8ac7f216f3e163
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-10 16:12:30 +0530

    Update package.json to use ES modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-console.log("Hello world");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-const express = require ('express');
+dotenv.config();
+
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
 //Create Server
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.listen(3000, ()=>{
+    console.log("Server is running in port 3000");
 })
 
 //Create Responce
