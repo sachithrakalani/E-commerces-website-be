080f99906704cfc4d70c82f2b96096e4d25883e4
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-11 12:08:28 +0530

    Create Database Connection using Postgres

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import pool from "./config/db.js";
 
 dotenv.config();
 
@@ -11,11 +12,17 @@ app.use(express.json());
 app.use(cors());
 
 //Create Server
-app.listen(3000, ()=>{
-    console.log("Server is running in port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server is running in port 3000");
+});
+
+// Testing POSTGRES Connection
+app.get("/", async (req, res) => {
+  console.log("Start");
+  const result = await pool.query("SELECT current_database()");
+  console.log("result", result.rows);
+  res.send(`The database name is : ${result.rows[0].current_database}`);
+});
 
 //Create Responce
-app.get("/",(req,res)=>[
-    res.send('Hello Node')
-]);
+app.get("/", (req, res) => [res.send("Hello Node")]);
