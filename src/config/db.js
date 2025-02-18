ab31d29f904530aa6dd67a8f1ffdde3f69527de0
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-18 22:15:18 +0530

    Create user table query

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
