b75da886b55222a14400c25afe14c9a7b23be3bd
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-19 12:19:53 +0530

    Create get user by id query

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -12,3 +12,8 @@ export const getAllUsersQuery = async () => {
   const result = await pool.query("SELECT * FROM users");
   return result.rows;
 };
+
+export const getUserByIdQuery = async (id) => {
+  const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+  return result.rows[0];
+};
