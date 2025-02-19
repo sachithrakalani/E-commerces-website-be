4621a60db962323a6a03283b590605f373f48cac
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-19 12:15:51 +0530

    Create get all user query

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,14 @@
 import pool from "../config/db.js";
 
 export const createUserQuery = async (username, email, password) => {
-    const result = await pool.query(
-      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING * ",
-      [username, email, password]
-    );
-    return result.rows[0];
-  };
+  const result = await pool.query(
+    "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING * ",
+    [username, email, password]
+  );
+  return result.rows[0];
+};
+
+export const getAllUsersQuery = async () => {
+  const result = await pool.query("SELECT * FROM users");
+  return result.rows;
+};
