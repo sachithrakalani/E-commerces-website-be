03abfd865a2c166f19a058ccc2910ff69b8a1417
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-20 10:24:21 +0530

    Create Admin table query

--- src/data/data.sql
+++ src/data/data.sql
@@ -4,4 +4,11 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(20) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
-)
+);
+
+CREATE TABLE IF NOT EXISTS admin (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(20) NOT NULL
+);
