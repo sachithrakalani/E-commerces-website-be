8eed15053289aa98e64a3de0ef7e06343c25162c
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-21 14:56:14 +0530

    Create Wines table query

--- src/data/data.sql
+++ src/data/data.sql
@@ -12,3 +12,12 @@ CREATE TABLE IF NOT EXISTS admin (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(20) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS wines (
+    id SERIAL PRIMARY KEY,
+    winename VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL, 
+    description TEXT,
+    manufacture_date DATE NOT NULL,
+    expiration_date DATE NOT NULL
+);
